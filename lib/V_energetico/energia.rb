bf8cbe203411837b537ee2de796a6f59a6399ba4
Gabriel Fdez <alu0100885453@ull.edu.es>
2017-10-26 19:43:24 +0000

    añadiendo esas funciones en la clase Energia

--- lib/V_energetico/energia.rb
+++ lib/V_energetico/energia.rb
@@ -7,5 +7,25 @@ class Energia
         @alimento, @proteinas, @glucidos, @lipidos, @valor_ener = a, b, c, d, []
     end
     
+    def to_s
+
+        out = "\t\t   Proteínas\tGlúcidos\tLípidos\n"
+            out << @alimento
+            out << "\t\t"
+            out << @proteinas.to_s
+            out << "\t\t"
+            out << @glucidos.to_s
+            out << "\t\t"
+            out << @lipidos.to_s
+            out << "\n"
+        out
+        
+    end
+    
+    def get_valor_energetico
+
+        return ((@proteinas + @glucidos) * 4 + @lipidos * 9).round(1)
+
+    end
     
 end
